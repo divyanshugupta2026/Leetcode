783378ad7d404ecf7183c48a747d9a4ea46a90a1
divyanshugupta2026 <divyanshu.gupta.professional@gmail.com>
2024-12-08 16:12:36 +0530

    Time: 4 ms (16.01%) | Memory: 13.1 MB (11.69%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.py
+++ 412-fizz-buzz/fizz-buzz.py
@@ -6,7 +6,7 @@ class Solution(object):
         """
         count=[]
         for i in range(1,n+1):
-            if i%5==0 and i%3==0:
+            if i%15==0:
                 a='FizzBuzz'
                 count.append(a)
             elif i%5==0:
